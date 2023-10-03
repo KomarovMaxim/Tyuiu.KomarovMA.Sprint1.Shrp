331cb11e917c27bbe4ab82b4c56391b4189087b5
Tyuiu.KomarovMA@outlook.com <Tyuiu.KomarovMA@outlook.com>
2023-10-03 18:22:47 +0500

    Add project files.

---
+++ Tyuiu.KomarovMA.Sprint1.Task0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KomarovMA.Sprint1.Task0.Lib
+{
+    public class DataService : ISprint1Task0V24
+    {
+        public double Calculate()
+        {
+            return (2.0f * 4.0f) / 4.0f / (2.0f + 1.0f);
+        }
+    }
+}
---
+++ Tyuiu.KomarovMA.Sprint1.Task0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMA.Sprint1.Task0.Lib;
+namespace Tyuiu.KomarovMA.Sprint1.Task0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+
+            Assert.AreEqual(0.6667, res, 0.0001);
+
+        }
+    }
+}
