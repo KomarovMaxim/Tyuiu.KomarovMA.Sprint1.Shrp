f0fa3a434913e2c05423da5d2da49ca44518c738
KomarovMaxim <144145045+KomarovMaxim@users.noreply.github.com>
2023-10-14 18:41:26 +0500

    Add files via upload

---
+++ Tyuiu.KomarovMA.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMA.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.KomarovMA.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CalculateTest()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(9, ds.Calculate(130985));
+        }
+    }
+}
---
+++ Tyuiu.KomarovMA.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMA.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.KomarovMA.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Комаров М.А. | СМАРТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Комаров Максим Александрович | СМАРТБ-23-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая присваивает целой переменной h третью от    *");
+            Console.WriteLine("* конца цифру в записи положительного целого числа k                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int k;
+            Console.WriteLine("Введите положительное число k:");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(k));
+
+            Console.ReadKey();
+        }
+    }
+}
